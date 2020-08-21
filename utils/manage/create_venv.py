a1aaa08dd7616934e61e17d097f49303426b5305
yolanda93h <yolanda93h@gmail.com>
2020-08-21 07:50:52 +0200

    windows compatible

--- utils/manage/create_venv.py
+++ utils/manage/create_venv.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import subprocess
 import venv
 
@@ -23,7 +24,12 @@ def create_venv(path_experiment, path_utils):
     # Paths
     path_venv = os.path.join(path_experiment, "venv")
     path_requirements = os.path.join(path_experiment, "requirements.txt")
-    path_python = os.path.join(path_venv, "bin", "python")
+
+    if platform.system() == 'Windows':
+        path_python = os.path.join(path_venv, "Scripts", "python")
+    else:
+        path_python = os.path.join(path_venv, "bin", "python")
+
     is_experiment(path_experiment)
     # Do not create a venv if found one
     if os.path.exists(path_venv):
@@ -40,11 +46,17 @@ def create_venv(path_experiment, path_utils):
     # Update pip to the last version
     print("\t- Upgrading local 'pip' package")
     subprocess.run([path_python, "-m", "pip", "install", "--upgrade", "pip"])
+
     # Install packages
     print("\t- Installing packages")
     subprocess.run([path_python, "-m", "pip", "install", "--requirement", path_requirements])
+
     # Link utils folder (venv have to exist)
-    path_dest_utils =  os.path.join(path_venv, 'lib', os.listdir(os.path.join(path_venv,'lib'))[0], 'site-packages', 'utils')
+    if platform.system() == 'Windows':
+        path_dest_utils =  os.path.join(path_venv, 'Lib', 'site-packages', 'utils')
+    else:
+        path_dest_utils = os.path.join(path_venv, 'lib', os.listdir(os.path.join(path_venv, 'lib'))[0], 'site-packages',
+                                       'utils')
     print("\t- Linking local utilities")
     os.symlink(path_utils, path_dest_utils, path_dest_utils) 
 
